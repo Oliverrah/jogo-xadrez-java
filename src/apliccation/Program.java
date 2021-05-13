ab442339cfea869170d2568a7726d685f1c79483
Oliverrah <oliverrahdeleonn@gmail.com>
2021-05-13 12:12:06 -0300

    Primeira classe: Position

--- src/apliccation/Program.java
+++ src/apliccation/Program.java
@@ -1,10 +1,14 @@
 package apliccation;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Testando");
+		
+		Position pos = new Position(3,5);
 
+		System.out.println(pos);
 	}
 
 }
