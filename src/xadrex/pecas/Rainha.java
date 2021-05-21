14917984b192cca0fe34a04ee2bd38953869c7ab
Oliverrah <oliverrahdeleonn@gmail.com>
2021-05-21 10:49:27 -0300

    Rainha

---
+++ src/xadrex/pecas/Rainha.java
@@ -0,0 +1,107 @@
+package xadrex.pecas;
+
+import tabuleirojogo.Posicao;
+import tabuleirojogo.Tabuleiro;
+import xadrez.Cor;
+import xadrez.PecaXadrez;
+
+public class Rainha extends PecaXadrez {
+
+	public Rainha(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro, cor);
+	}
+
+	@Override
+	public String toString() {
+		return "D";
+	}
+
+	@Override
+	public boolean[][] movimentosPossiveis() {
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posicao p = new Posicao(0, 0);
+
+		// acima
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// esquerda
+		p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// direita
+		p.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() + 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// abaixo
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// diagonal esquerda cima
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// diagonal direita cima
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// diagonal esquerda baixo
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// diagonal direita baixo
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		while (getTabuleiro().existeEssaPosicao(p) && !getTabuleiro().temAlgumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().existeEssaPosicao(p) && temAlgumaPecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		return mat;
+	}
+}
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -12,6 +12,7 @@ import tabuleirojogo.Tabuleiro;
 import xadrex.pecas.Bispo;
 import xadrex.pecas.Cavalo;
 import xadrex.pecas.Peao;
+import xadrex.pecas.Rainha;
 import xadrex.pecas.Rei;
 import xadrex.pecas.Torre;
 
@@ -201,6 +202,7 @@ public class PartidaXadrez {
         colocarNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCA));
         colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCA));
         colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCA));
+        colocarNovaPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCA));
         colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCA));
         colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCA));
         colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCA));
@@ -217,6 +219,7 @@ public class PartidaXadrez {
         colocarNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETA));
         colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETA));
         colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETA));
+        colocarNovaPeca('d', 8, new Rainha(tabuleiro, Cor.PRETA));
         colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETA));
         colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETA));
         colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETA));
